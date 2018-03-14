1fb2459cc01f7561c0a4cb686c32b701cc5e5e5e
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-03-14 01:21:49 -0400

    powershell: use C# var

--- win/Documents/WindowsPowershell/profile.cs
+++ win/Documents/WindowsPowershell/profile.cs
@@ -17,8 +17,7 @@ public static class Profile
 {
     public static bool IsAdmin()
     {
-        WindowsIdentity user = WindowsIdentity.GetCurrent();
-        WindowsPrincipal group = new WindowsPrincipal(user);
+        var group = new WindowsPrincipal(WindowsIdentity.GetCurrent());
         return group.IsInRole(WindowsBuiltInRole.Administrator);
     }
 }
