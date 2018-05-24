f2d381add911c1b5482d644e89ac63548ba20d5a
Jan Edmund Lazo <janedmundlazo@hotmail.com>
2018-05-24 00:35:31 -0400

    powershell: Set-StrictMode

--- win/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
+++ win/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+Set-StrictMode -Version Latest
+
 & {
     foreach ($ext in @('dll', 'cs')) {
         $path = "$PSScriptRoot/profile.$ext"
