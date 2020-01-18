352e0d30bec767d8acf057de00f826da54f567a6
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2020-01-18 18:32:36 -0500

    ci/travis: minimal image, run shellcheck via docker

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -ex
 cd "$(dirname "$0")/.."
 bash -n unix/.bash*
-shellcheck -s bash unix/.bash*
+docker pull 'koalaman/shellcheck:v0.7.0'
+docker run --rm -v "$PWD:/mnt" 'koalaman/shellcheck:v0.7.0' -s bash unix/.bash*
